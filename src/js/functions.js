93cda25f2aeb547e8fbfeee8657048ab64ead987
waynegerard <WayneGerard@gmail.com>
2012-01-28 07:48:07 -0800

    update

--- src/js/functions.js
+++ src/js/functions.js
@@ -23,7 +23,10 @@ var nameToIDMapKey = "nameToID";
 ////////////////////
 
 function getNewGameID() {
-    return "1";
+    var S4 = function() {
+       return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    };
+    return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
 }
 
 function getGameID() {
