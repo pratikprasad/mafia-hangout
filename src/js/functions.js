537b6ca8d2ecaa338cb606caf7a8557e8f70dbb1
waynegerard <WayneGerard@gmail.com>
2012-01-28 07:39:28 -0800

    update

--- src/js/functions.js
+++ src/js/functions.js
@@ -357,7 +357,7 @@ function stateChanged(delta, metadata) {
     console.log("received update for state with delta: ", delta);
 
     _state = delta.state;
-    if ("gameID" == delta.addedKeys[0].key) { // Starting a new game, ask for our role
+    if (delta.addedKeys.length > 0 && "gameID" == delta.addedKeys[0].key) { // Starting a new game, ask for our role
 	console.log("Starting new game");
 	askForRole();
 	addSelfToReverseMap();
